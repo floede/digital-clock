ae85bc77e0177a38090b1e064b3b666bb738a11f
Janus Hasseriis <floede@gmail.com>
2017-01-09 00:04:46 +0100

    Rolex style

--- babel/index.js
+++ babel/index.js
@@ -6,7 +6,7 @@ var moment = require('moment');
 
 class Clock {
     constructor() {
-        this.setTime = this.setTime.bind(this);
+        //this.setTime = this.setTime.bind(this);
     }
 
     getDegrees(value, format) {
@@ -21,22 +21,22 @@ class Clock {
     setHour () {
         var hourNow = this.now.hour();
         var hourNow = hourNow > 12 ? (hourNow - 12) : hourNow;
-        this.setDegree(hour, this.getDegrees(hourNow, 12));
+        this.setDegree(hour, this.getDegrees(hourNow, 12) + (this.now.minute()/2));
     }
     setMinute () {
         var minNow = this.now.minute();
-        this.setDegree(minute, this.getDegrees(minNow, 60));
+        this.setDegree(minute, this.getDegrees(minNow, 60) + (this.now.second()/10));
     }
     setSecond () {
         var secNow = this.now.second();
-        this.setDegree(second, this.getDegrees(secNow, 60));
+        this.setDegree(second, this.getDegrees(secNow, 60) + (this.now.milliseconds()*(6/1000)));
     }
     setTime() {
         this.now = moment();
         this.setHour();
         this.setMinute();
         this.setSecond();
-        setTimeout(this.setTime, 1000);
+        setTimeout(() => this.setTime(), 1);
     }
 };
 
